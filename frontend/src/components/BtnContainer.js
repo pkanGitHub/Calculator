157cfa3fd26fe8879569618bd604328df233fc59
Phyllis Kan <pyk9q7@gmail.com>
2022-05-23 13:26:40 -0500

    set up components

---
+++ frontend/src/components/BtnContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const BtnContainer = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default BtnContainer
---
+++ frontend/src/components/Display.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Display = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default Display
